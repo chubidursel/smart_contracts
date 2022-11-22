b97d71902417874d271b01106d33d8bd06c45040
chubidursel <baurimk21@gmail.com>
2022-11-21 20:50:06 -0400

    others

---
+++ tools/stringComprasion.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+contract StringComparison  { 
+
+function comareStr(string calldata a, string calldata b) public pure returns (bool) {
+    if(bytes(a).length != bytes(b).length) {
+        return false;
+    } else {
+      return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
+    }
+}
+}
+
